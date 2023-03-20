0aab1437cdf969b7d29e61f990d70b745e610f9c
Riley Snyder <riley.snyder@harness.io>
2023-03-20 16:30:46 -0500

    message

--- modules/user_accounts/outputs.tf
+++ modules/user_accounts/outputs.tf
@@ -3,6 +3,9 @@
 # Harness User Account Outputs
 #
 ####################
+# 2023-03-16
+# This output is being deprecated and replaced by the output
+# labeled `details`
 output "user_account_details" {
   value       = harness_platform_user.user
   description = "Details for the created Harness User Account"
--- modules/user_groups/outputs.tf
+++ modules/user_groups/outputs.tf
@@ -3,6 +3,9 @@
 # Harness User Group Outputs
 #
 ####################
+# 2023-03-16
+# This output is being deprecated and replaced by the output
+# labeled `details`
 output "user_group_details" {
   value       = harness_platform_usergroup.group
   description = "Details for the created Harness User Group"
